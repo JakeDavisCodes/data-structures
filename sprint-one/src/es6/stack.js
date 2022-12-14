30602cb785a19ddff739a1347458b75caf13d81a
Jake <jakedavisemail@gmail.com>
2022-12-14 13:23:23 -0700

    Finish ES6 Stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.top = 0;
+  }
+
+  push(value) {
+    this.top++;
+    this.storage[this.top] = value;
+  }
+
+  pop() {
+    let ret = this.storage[this.top];
+    delete this.storage[this.top];
+    this.top--;
+    return ret;
+  }
+
+  size() {
+    if (this.top < 0) {
+      return 0;
+    }
+    return this.top;
   }
 
 }
